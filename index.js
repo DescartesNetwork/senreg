ff6e757faf6e5042a9be5596aa6763d31fc571ca
tuphan-dn <tuphan@descartes.network>
2021-11-03 16:12:49 +0700

    _

---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const path = require('path')
+
+const src = path.join(__dirname, './src')
+function validate(manifest) {
+  const rule = /^[A-Za-z0-9]*$/
+  if (!manifest || typeof manifest != 'object') return false
+  if (!manifest.appId || !rule.test(manifest.appId)) return false
+  if (!manifest.url || !manifest.name || !manifest.description) return false
+  if (!manifest.author || !manifest.author.name || !manifest.author.email)
+    return false
+  return true
+}
+
+const register = {}
+fs.readdirSync(src).forEach(function (file) {
+  const manifest = require(`./src/${file}`)
+  if (!validate(manifest)) throw new Error('Invalid manifest.')
+  if (file != `${manifest.appId}.json`)
+    throw new Error(
+      'Invalid name. The file name must be identical to the app id.',
+    )
+  register[manifest.appId] = manifest
+})
+fs.writeFileSync('register.json', JSON.stringify(register, null, 2))
+
+console.log('********************************')
+console.log(
+  `There are ${Object.keys(register).length} applications have been built!`,
+)
+console.log('********************************')
