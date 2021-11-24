cddb3ce078963ff339009980e97b9cc427a3fabd
tuphan-dn <tuphan@descartes.network>
2021-11-24 23:16:23 +0700

    _

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const path = require('path')
 
 const src = path.join(__dirname, './src')
 function validateFilename(filename) {
-  const rule = /^([A-Za-z0-9\-]*)\.manifest\.json$/
+  const rule = /^([A-Za-z0-9\_]*)\.manifest\.json$/
   if (!rule.test(filename)) return false
   return true
 }
 function validateManifest(manifest) {
-  const rule = /^[A-Za-z0-9\-]*$/
+  const rule = /^[A-Za-z0-9\_]*$/
   if (!manifest || typeof manifest != 'object') return false
   if (!manifest.appId || !rule.test(manifest.appId)) return false
   if (!manifest.url || !manifest.name || !manifest.description) return false
+  if (manifest.name.toLowerCase().replace(' ', '_') !== manifest.appId)
+    return false
   if (!manifest.author || !manifest.author.name || !manifest.author.email)
     return false
   return true
